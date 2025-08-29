fa16fa2a36cf2f8b0d8b28a1fd23f43bf5a2f47a
OttoVPCC <ottojackson03@gmail.com>
2025-08-29 11:13:26 -0400

    lots of changes

--- FinalTest/FinalTest.cpp
+++ FinalTest/FinalTest.cpp
@@ -1,9 +1,19 @@
 //Test to see if GitHub is actually saving my changes
 #include <iostream>
-int main()
+using namespace std;
+
+void main()
 {
-    std::cout << "Hello World! This is a test to see if my code works properly!\n";
+	int number1 = 0 , number2 = 0;
+	int sum;
+	cout << "Please enter your first number";
+	cin >> number1;
+	cout << "Pleate enter your second number";
+	cin >> number2;
+	sum = number1 + number2;
+		// changing from addition to subtraction does not break this
+	cout << "The sum of " << number1 << " and " << number2 << " is: " << sum;
 }
 //Test to see if GitHub is actually saving my changes
 //Horray! I figured out how to put comments in my code, surely I will not abuse this feature.
-//this should just be for a branch??
+//this should just be for a branch
