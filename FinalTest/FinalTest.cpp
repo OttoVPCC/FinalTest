5d2a1ebd5613ad446cc26d1eaa4610d5bf0c10f9
OttoVPCC <ottojackson03@gmail.com>
2025-08-29 10:41:25 -0400

    little edits

--- FinalTest/FinalTest.cpp
+++ FinalTest/FinalTest.cpp
@@ -1,10 +1,8 @@
-
-
+//Test to see if GitHub is actually saving my changes
 #include <iostream>
-
 int main()
 {
     std::cout << "Hello World! This is a test to see if my code works properly!\n";
 }
-
 //Test to see if GitHub is actually saving my changes
+//Horray! I figured out how to put comments in my code, surely I will not abuse this feature.
